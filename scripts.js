f126fc661190ee13a944e927f9071e7d0806375d
Przemysław Wróbel <przemek.wrobel@poczta.fm>
2018-01-30 19:52:25 +0100

    poprawiony skrypt js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
-var a = prompt ("Podaj długość podstawy trójkąta a = ");
-	h = prompt ("Podaj wysokość trójkąta h = ");
-	triangleArea = 0;
+var a = prompt ("Podaj długość podstawy trójkąta a = "),
+	h = prompt ("Podaj wysokość trójkąta h = "),
+	triangleArea = 0.5*a*h;
 
-alert('triangleArea, ' + 0.5*a*h);
+alert('triangleArea, ' + triangleArea); 
 
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
 // 
